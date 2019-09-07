b8e4eb5e750c25063590fbe176b6bf163bd17cef
John Grib <johngrib82@gmail.com>
2019-09-07 14:42:56 +0900

    정적 리소스(css, js, png..)를 추가하라

--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package todoapp.web;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -9,23 +10,30 @@ import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
  *
  * @author springrunner.kr@gmail.com
  */
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        // ContentNegotiating 관련 설정은 ContentNegotiationCustomizer를 통해 해야한다.
-        // 여기서 직접 설정하면, 스프링부트가 구성한 ContentNegotiating 설정이 무시된다.
-    }
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/assets/**")
+//				.addResourceLocations("assets/", "classpath:assets/");
+//	}
 
-    /**
-     * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성
-     */
-    public static class ContentNegotiationCustomizer {
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// ContentNegotiating 관련 설정은 ContentNegotiationCustomizer를 통해 해야한다.
+		// 여기서 직접 설정하면, 스프링부트가 구성한 ContentNegotiating 설정이 무시된다.
+	}
 
-        public void configurer(ContentNegotiatingViewResolver viewResolver) {
-            // TODO ContentNegotiatingViewResolver 사용자 정의
-        }
+	/**
+	 * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성
+	 */
+	public static class ContentNegotiationCustomizer {
 
-    }
+		public void configurer(ContentNegotiatingViewResolver viewResolver) {
+			// TODO ContentNegotiatingViewResolver 사용자 정의
+		}
+
+	}
 
 }
