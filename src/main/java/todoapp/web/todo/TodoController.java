57efe90aa7fe7d18c7a95ca5a052cfb2c8af749e
John Grib <johngrib82@gmail.com>
2019-09-07 15:09:40 +0900

    사이트 기본 정보가 각 화면에 나타나게 하라

--- src/main/java/todoapp/web/todo/TodoController.java
+++ src/main/java/todoapp/web/todo/TodoController.java
@@ -1,20 +1,32 @@
 package todoapp.web.todo;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import todoapp.web.model.SiteProperties;
 
 @Controller
 public class TodoController {
-	
-		@RequestMapping("/todos")
-		public ModelAndView todos() {
-			
-			// ThymeleafViewResolver, Spring boot가 Spring MVC에게 등록 처리 
-			// 머릿글 => classpath:templates/, 꼬릿글=> .html
-			ModelAndView mav = new ModelAndView("todos");
-			
-			return mav;
-		}
+
+	private Environment env;
+	private SiteProperties properties;
+
+	public TodoController(Environment env, SiteProperties properties) {
+		this.env = env;
+		this.properties = properties;
+	}
+
+	@RequestMapping("/todos")
+	public ModelAndView todos() {
+
+		// ThymeleafViewResolver, Spring boot가 Spring MVC에게 등록 처리
+		// 머릿글 => classpath:templates/, 꼬릿글=> .html
+
+		ModelAndView mav = new ModelAndView("todos");
+		mav.addObject("site", properties);
+
+		return mav;
+	}
 }
