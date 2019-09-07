db1b07aeb03c5337c667cb8732d9064ca183f209
John Grib <johngrib82@gmail.com>
2019-09-07 16:41:50 +0900

    할 일 문자열이 올바른지 검증하라

--- src/main/java/todoapp/core/todos/domain/Todo.java
+++ src/main/java/todoapp/core/todos/domain/Todo.java
@@ -13,121 +13,125 @@ import java.util.Objects;
 @Entity
 public class Todo {
 
-    @Id
-    private Long id;
-    private String title;
-    private TodoState state = TodoState.ACTIVE;
+	@Id
+	private Long id;
+	private String title;
+	private TodoState state = TodoState.ACTIVE;
 
-    private String username;
+	private String username;
 
-    private LocalDateTime createdDate = LocalDateTime.now();
-    private LocalDateTime lastModifiedDate = createdDate;
+	private LocalDateTime createdDate = LocalDateTime.now();
+	private LocalDateTime lastModifiedDate = createdDate;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    private void setId(Long id) {
-        this.id = id;
-    }
+	private void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    private void setTitle(String title) {
-        this.title = title;
-    }
+	private void setTitle(String title) {
+		if (!Objects.nonNull(title) || title.trim().length() < 4) {
+			throw new TodoCreationException("할 일은 최소 4자 이상 작성해야 합니다.");
+		}
+		this.title = title;
+	}
 
-    public TodoState getState() {
-        return state;
-    }
+	public TodoState getState() {
+		return state;
+	}
 
-    private void setState(TodoState state) {
-        this.state = state;
-    }
+	private void setState(TodoState state) {
+		this.state = state;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    private void setUsername(String username) {
-        this.username = username;
-    }
+	private void setUsername(String username) {
+		this.username = username;
+	}
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
 
-    private void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
+	private void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
 
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
+	public LocalDateTime getLastModifiedDate() {
+		return lastModifiedDate;
+	}
 
-    private void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
+	private void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
 
-    public boolean isCompleted() {
-        return state == TodoState.COMPLETED;
-    }
+	public boolean isCompleted() {
+		return state == TodoState.COMPLETED;
+	}
 
-    public Todo update(String title, boolean completed) {
-        setTitle(title);
-        setState(completed ? TodoState.COMPLETED : TodoState.ACTIVE);
-        setLastModifiedDate(LocalDateTime.now());
-        return this;
-    }
+	public Todo update(String title, boolean completed) {
+		setTitle(title);
+		setState(completed ? TodoState.COMPLETED : TodoState.ACTIVE);
+		setLastModifiedDate(LocalDateTime.now());
+		return this;
+	}
 
-    public Todo update(String title, boolean completed, String username) {
-        return verifyUsername(username).update(title, completed);
-    }
+	public Todo update(String title, boolean completed, String username) {
+		return verifyUsername(username).update(title, completed);
+	}
 
-    private Todo verifyUsername(String username) {
-        if (!Objects.equals(username, this.username)) {
-            throw new TodoUsernameNotMatchedException();
-        }
-        return this;
-    }
+	private Todo verifyUsername(String username) {
+		if (!Objects.equals(username, this.username)) {
+			throw new TodoUsernameNotMatchedException();
+		}
+		return this;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId());
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Todo todo = (Todo) o;
-        return Objects.equals(getId(), todo.getId());
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Todo todo = (Todo) o;
+		return Objects.equals(getId(), todo.getId());
+	}
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Todo{");
-        sb.append("id=").append(id);
-        sb.append('}');
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Todo{");
+		sb.append("id=").append(id);
+		sb.append('}');
+		return sb.toString();
+	}
 
+	public static Todo create(String title) {
+		Todo todo = new Todo();
+		todo.setId(System.nanoTime());
+		todo.setTitle(title);
+		return todo;
+	}
 
-    public static Todo create(String title) {
-        Todo todo = new Todo();
-        todo.setId(System.nanoTime());
-        todo.setTitle(title);
-        return todo;
-    }
-
-    public static Todo create(String title, String username) {
-        Todo todo = new Todo();
-        todo.setId(System.nanoTime());
-        todo.setTitle(title);
-        todo.setUsername(username);
-        return todo;
-    }
+	public static Todo create(String title, String username) {
+		Todo todo = new Todo();
+		todo.setId(System.nanoTime());
+		todo.setTitle(title);
+		todo.setUsername(username);
+		return todo;
+	}
 
 }
