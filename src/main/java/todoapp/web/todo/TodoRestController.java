61b4decdbbec728c2c0264ece3165bd3e3dcbf32
John Grib <johngrib82@gmail.com>
2019-09-07 16:17:03 +0900

    할 일을 추가하는 기능을 구현하라

--- src/main/java/todoapp/web/todo/TodoRestController.java
+++ src/main/java/todoapp/web/todo/TodoRestController.java
@@ -2,19 +2,29 @@ package todoapp.web.todo;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import todoapp.core.todos.application.TodoEditor;
 import todoapp.core.todos.application.TodoFinder;
 import todoapp.core.todos.domain.Todo;
 
 @RestController
 public class TodoRestController {
 
+	private final Logger log = LoggerFactory.getLogger(TodoRestController.class);
 	private TodoFinder todoFinder;
+	private TodoEditor todoEditor;
 
-	public TodoRestController(TodoFinder todoFinder) {
+	public TodoRestController(TodoFinder todoFinder, TodoEditor todoEditor) {
 		this.todoFinder = todoFinder;
+		this.todoEditor = todoEditor;
 	}
 
 	@GetMapping("/api/todos")
@@ -22,4 +32,24 @@ public class TodoRestController {
 		return todoFinder.getAll();
 	}
 
+	@PostMapping("/api/todos")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void create(@RequestBody TodoWriteCommand command) {
+		log.debug("title: {}", command.getTitle());
+		todoEditor.create(command.getTitle());
+	}
+
+	// Command 라는 이름은 spring 2.5부터 내려온 규약
+	static class TodoWriteCommand {
+		private String title;
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+	}
 }
