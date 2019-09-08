eb81b6b41b8630b1b3f519cb5a18385dfa53d20a
John Grib <johngrib82@gmail.com>
2019-09-08 14:12:11 +0900

    잘못된 로그인 요청을 처리하라

--- src/main/java/todoapp/web/user/LoginController.java
+++ src/main/java/todoapp/web/user/LoginController.java
@@ -3,12 +3,15 @@ package todoapp.web.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import todoapp.core.user.application.UserJoinder;
 import todoapp.core.user.application.UserPasswordVerifier;
+import todoapp.core.user.domain.UserEntityNotFoundException;
+import todoapp.core.user.domain.UserPasswordNotMatchedException;
 
 @Controller
 @RequestMapping("/login")
@@ -27,17 +30,24 @@ public class LoginController {
     public void loginForm() {
     }
 
-    /**
-     * 사용자 저장소에 사용자가 있을 경우: 비밀번호 일치 확인
-     *      비밀번호가 일치하면: /todos 리다이렉트
-     *      비밀번호가 다르면: login 페이지로 돌려보내고, 오류 메시지 노출
-     * 사용자가 없는 경우: 회원가입 페이지로 돌려보냄
-     */
     @PostMapping
-    public String loginProcess(LoginCommand command) {
+    public String loginProcess(LoginCommand command, Model model) {
         log.debug("username: {}, password: {}", command.getUsername(), command.getPassword());
 
-        return "login";
+        try {
+            // 사용자 저장소에 사용자가 있을 경우: 비밀번호 일치 확인
+            // 비밀번호가 일치하면: /todos 리다이렉트
+            verifier.verify(command.getUsername(), command.getPassword());
+        } catch (UserPasswordNotMatchedException e) {
+            // 비밀번호가 다르면: login 페이지로 돌려보내고, 오류 메시지 노출
+            model.addAttribute("message", e.getMessage());
+            return "login";
+        } catch (UserEntityNotFoundException e) {
+            // 사용자가 없는 경우: 회원가입 페이지로 돌려보냄
+            joinder.join(command.getUsername(), command.getPassword());
+        }
+
+        return "redirect:/todos";
     }
     
     static class LoginCommand {
