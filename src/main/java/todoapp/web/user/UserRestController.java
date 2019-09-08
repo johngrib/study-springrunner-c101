c28eb692bab55f6b4dc9ebcbe136a22486a7a80d
John Grib <johngrib82@gmail.com>
2019-09-08 17:36:11 +0900

    사용자 사진 업로드를 접수하라

--- src/main/java/todoapp/web/user/UserRestController.java
+++ src/main/java/todoapp/web/user/UserRestController.java
@@ -2,18 +2,30 @@ package todoapp.web.user;
 
 import javax.annotation.security.RolesAllowed;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import todoapp.security.UserSession;
 import todoapp.web.model.UserProfile;
 
+@RolesAllowed({ "ROLE_USER" })
 @RestController
 public class UserRestController {
 
-    @RolesAllowed({ "ROLE_USER" })
+    private final Logger log = LoggerFactory.getLogger(UserRestController.class);
+
     @GetMapping("/api/user/profile")
     public UserProfile userProfile(UserSession session) {
+        return new UserProfile(session.getUser());
+    }
+
+    @PostMapping("/api/user/profile-picture")
+    public UserProfile changePorfilePicture(MultipartFile profilePicture, UserSession session) {
+        log.debug("profilePicture: {}", profilePicture);
         return new UserProfile(session.getUser());
     }
 }
